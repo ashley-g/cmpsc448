402d003a6da070e34be18612c842475b65d15961
Ashley Gallagher <gllghr11@gmail.com>
2020-09-14 12:40:59 -0400

    finalize hw1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -5,13 +5,17 @@ mat = [[3, 1, 1], [2, 4, 2], [-1, -1, 1]]
 #get eigendecomposition of mat
 decomp = numpy.linalg.eig(mat)
 print(decomp)
-print("\nEigen vals: {0}, {1}, {2}".format(decomp[0][0], decomp[0][1], decomp[0][2]))
-print("\nEigen vectors: \n {0}".format(decomp[1]))
+print("\nEigen vals: {0}\n".format(decomp[0]))
+print("Eigen vectors: \n {0}\n".format(decomp[1]))
 
 #attempt to reconstruct mat uisng decomp
 inv = numpy.linalg.inv(decomp[1])
 vals = [[decomp[0][0], 0, 0], [0, decomp[0][1], 0], [0, 0, decomp[0][2]]]
 prod = numpy.matmul(decomp[1], vals)
 prod = numpy.matmul(prod, inv)
-print("\nReconstruction:\n {0}".format(prod))
+print("Reconstruction:\n {0}\n".format(prod))
 
+#question 1 find the rank
+mat = [[1, 2, 1], [1, 0, 3], [1, 1, 2]]
+rank = numpy.linalg.matrix_rank(mat)
+print("Rank: " + str(rank))
